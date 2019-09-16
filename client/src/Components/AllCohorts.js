a81cce4d2c96a0aa9650bfdc858f000896982d7d
Tommy Liang <tommyliangsays@gmail.com>
2019-09-16 14:35:48 -0400

    updated login and allcohorts

--- client/src/Components/AllCohorts.js
+++ client/src/Components/AllCohorts.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import '../Css/AllCohorts.css';
 
 function AllCohorts() {
     return(
       <div className="AllCohorts">
-        <p> This is main - This is 3 cohorts </p>
+
+        <div className="cohortContainer">
+          <h3 className="cohortName"> 
+            <Link to='/cohort'>Pirates</Link> 
+          </h3>
+        </div>
+
+        <div className="cohortContainer">
+          <h3 className="cohortName"> 
+            <Link to='/cohort'>Cohort 1</Link> 
+          </h3>
+        </div>
+        
+        <div className="cohortContainer">
+          <h3 className="cohortName"> 
+            <Link to='/cohort'>Cohort 2</Link> 
+          </h3>
+        </div>
       </div>
     )
 }
--- client/src/Components/Nav.js
+++ client/src/Components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Nav() {
     return(
       <nav className="NavComponent">
-        <h1> This is nav- GA </h1>
+        <h1> This is nav - GA </h1>
       </nav>
     )
 }
