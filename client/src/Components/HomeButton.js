59dc8bc803a6cda31b06d5ae45bd17bb69bfb292
Joshua D Dyer <josh@Big-Mac.local>
2019-09-17 11:38:37 -0400

    built homebutton component and styled it

---
+++ client/src/Components/HomeButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Css/HomeButton.css'
+
+function HomeButton() {
+    return(
+      <div className="HomeButton">
+        <a href="/">
+          Home
+        </a>
+      </div>
+    )
+}
+
+export default HomeButton;
