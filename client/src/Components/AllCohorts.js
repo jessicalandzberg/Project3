d2925f1294d1251fd5ae388d7e8c09bb8c23105c
Joshua D Dyer <josh@Big-Mac.local>
2019-09-17 10:18:15 -0400

    worked on allcohort css

--- client/src/Components/AllCohorts.js
+++ client/src/Components/AllCohorts.js
@@ -2,28 +2,29 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import '../Css/AllCohorts.css';
 
+
 function AllCohorts() {
     return(
       <div className="AllCohorts">
+        <div className="AllCohortContainer">
+          <div className="cohortContainer">
+            <h3 className="cohortName">
+              <Link to='/cohort'>Pirates</Link>
+            </h3>
+          </div>
 
-        <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Pirates</Link> 
-          </h3>
-        </div>
+          <div className="cohortContainer" >
+            <h3 className="cohortName">
+              <Link to='/cohort'>Cohort 1</Link>
+            </h3>
+          </div>
 
-        <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Cohort 1</Link> 
-          </h3>
+          <div className="cohortContainer">
+            <h3 className="cohortName">
+              <Link to='/cohort'>Cohort 2</Link>
+            </h3>
+          </div>
         </div>
-        
-        <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Cohort 2</Link> 
-          </h3>
-        </div>
-      
       </div>
     )
 }
