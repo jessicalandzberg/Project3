3293258311d9517430d105f5728f44e0b4eaf95b
Jessica Landzberg <jessica.landzberg@gmail.com>
2019-09-17 15:40:28 -0400

    a bunch of different edits to api helper

--- client/src/Components/SingleCohort.js
+++ client/src/Components/SingleCohort.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import '../Css/SingleCohort.css';
+import { showStudents, destroyStudent } from '../Services/api-helper.js'
 
 
-function SingleCohort(props) {
-  console.log(props);
+class SingleCohort extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [],
+      activeStudent: []
+      }
+    }
+
+  getStudents = async () => {
+    const cohort_id = this.props.match.params.id
+    const students = await showStudents(cohort_id);
+    this.setState({ students })
+    };
+
+  deleteStudent = async (id) => {
+    await destroyStudent(id);
+    this.setState((prevState) => ({
+      students: [...prevState.students.filter((stud) => stud.id !== id)]
+    }));
+  }
+
+  componentDidMount() {
+    this.getStudents();
+  }
+
+  handleClick= (d) => {
+    const activeStudent = d
+    this.setState(prevState => ({
+      activeStudent: activeStudent
+    }))
+  }
+
+
+  render () {
+    console.log(this.props)
+    const AllStudents = this.state.students.map((d,i) => {
+        return (<div key={i}>
+          <div onClick={()=>this.handleClick(d)}> {d.name} </div>
+          <button onClick={() => this.deleteStudent(d.id)}>DELETE</button>
+        </div>)
+      })
     return(
       <div className="SingleCohort">
         <h3 className="header"> XXX Cohort </h3>
@@ -12,21 +54,18 @@ function SingleCohort(props) {
 
           <div className="StudentList">
             <h4> Student List </h4>
-            <div> Jess </div>
-            <div> JC </div>
-            <div> Tommy </div>
-            <div> Josh </div>
+            {AllStudents}
           </div>
 
           <div className="singleStudent">
-            <h4> Name </h4>
-            <div> Cohort: </div>
-            <div> Linkedin: </div>
-            <div> Fun fact: </div>
+            <h4> Name: {this.state.activeStudent.name} </h4>
+            <div> Cohort Id: {this.state.activeStudent.cohortId} </div>
+            <a target='_blank' href={this.state.activeStudent.linkedin}> Linkedin profile </a>
           </div>
         </div>
       </div>
     )
+  }
 }
 
 export default SingleCohort;
