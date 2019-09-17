d51b7e0a9f3b1ab1d05f229978c9eb6dc376cc15
J.C. Dorr <jcdorr3@JCs-MacBook-Pro.local>
2019-09-17 10:22:48 -0400

    finished api-helper and worked on main and allCohorts.

--- client/src/Components/AllCohorts.js
+++ client/src/Components/AllCohorts.js
@@ -7,23 +7,23 @@ function AllCohorts() {
       <div className="AllCohorts">
 
         <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Pirates</Link> 
+          <h3 className="cohortName">
+            <Link to='/cohort/1'>Pirates</Link>
           </h3>
         </div>
 
         <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Cohort 1</Link> 
+          <h3 className="cohortName">
+            <Link to='/cohort/2'>Cohort 1</Link>
           </h3>
         </div>
-        
+
         <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Cohort 2</Link> 
+          <h3 className="cohortName">
+            <Link to='/cohort/3'>Cohort 2</Link> 
           </h3>
         </div>
-      
+
       </div>
     )
 }
---
+++ client/src/Services/api-helper.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+
+const BASE_URL = 'http://localhost:3001';
+
+export const showStudents = async () => {
+  try {
+    const students = await axios.get(`${BASE_URL}/cohorts`);
+    console.log(students.data);
+    return students.data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export const createStudent = async (data) => {
+  try {
+    const cohort_id = 
+    const student = await axios.post(`${BASE_URL}/cohorts/${cohort_id}`, data);
+    console.log(student.data);
+    return student.data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export const updateStudent = async (data, id) => {
+  try {
+    const student = await axios.put(`${BASE_URL}/cohorts/${id}`, data);
+    console.log(student.data);
+    return student.data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+export const destroyStudent = async (id) => {
+  try {
+    const student = await axios.delete(`${BASE_URL}/cohorts/${id}`);
+    console.log(student.data);
+    return student.data;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
