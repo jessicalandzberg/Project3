5148b4eee2d51e806be312975abf9dcb2a69db78
Tommy Liang <tommyliangsays@gmail.com>
2019-09-17 10:11:10 -0400

    added nav img

--- client/src/Components/AllCohorts.js
+++ client/src/Components/AllCohorts.js
@@ -6,25 +6,27 @@ function AllCohorts() {
     return(
       <div className="AllCohorts">
 
-        <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Pirates</Link> 
-          </h3>
-        </div>
+      <h1 className="cohortTitle">Choose A Cohort</h1>
+        <div AllCohortContainer>
+          <div className="cohortContainer">
+            <h3 className="cohortName"> 
+              <Link to='/cohort'>Pirates</Link> 
+            </h3>
+          </div>
 
-        <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Cohort 1</Link> 
-          </h3>
-        </div>
-        
-        <div className="cohortContainer">
-          <h3 className="cohortName"> 
-            <Link to='/cohort'>Cohort 2</Link> 
-          </h3>
-        </div>
-      
+          <div className="cohortContainer">
+            <h3 className="cohortName"> 
+              <Link to='/cohort'>Cohort 1</Link> 
+            </h3>
+          </div>
+          
+          <div className="cohortContainer">
+            <h3 className="cohortName"> 
+              <Link to='/cohort'>Cohort 2</Link> 
+            </h3>
+          </div>
       </div>
+    </div>
     )
 }
 
