8bd169186aeed819bd2d36bf1a53589704fec3e4
Tommy Liang <tommyliangsays@gmail.com>
2019-09-18 11:49:08 -0400

    working on login

--- client/src/Components/LogIn.js
+++ client/src/Components/LogIn.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import '../Css/LogIn.css';
 
-const toggleDisplay = () => {
-
-  console.log('this is clicked!!!!')
-}
-
 function LogIn() {
   return(
     <div className="LogIn">
-
       <div className="loginCreateContainer">
+        <div className="overlay">
         <div className ="logInContainer">
           <h3>Login</h3>
           <form className="logInForm">
             <input type="text" placeholder="Username" className="field"/>
-          <input type="password" placeholder="Password" className="field"/>
+          <input type="text" placeholder="Password" className="field"/>
           <input type="submit" value="login" className="btn"/>
           </form>
         </div>
@@ -24,12 +19,12 @@ function LogIn() {
           <h3>Create User</h3>
           <form className="createForm">
             <input type="text" placeholder="Username" className="field"/>
-          <input type="password" placeholder="Password" className="field"/>
+          <input type="text" placeholder="Password" className="field"/>
         <input type="submit" value="create" className="btn"/>
           </form>
         </div>
       </div>
-
+    </div>
   </div>
   )
 }
