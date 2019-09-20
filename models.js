f4012d7fe8e7ab25b23841579d11953572cbd732
Jessica Landzberg <jessica.landzberg@gmail.com>
2019-09-20 11:41:05 -0400

    made changes for heroku

--- models.js
+++ models.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize({
+const db = new Sequelize(
+  (process.env.DATABASE_URL || 'postgres://localhost:5432/GASei_db'),
+  {
   database: 'GASei_db',
   dialect: 'postgres',
   define: {
